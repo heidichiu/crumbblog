93b29944074be9a135b3d7263ed3fe95a94f6df0
heidichiu <heidi@xqtem.com>
2021-01-24 22:27:19 +0800

    Add user password hashing

--- crumbblog-backend/src/main/java/com/crumbblog/crumbblog/user/UserService.java
+++ crumbblog-backend/src/main/java/com/crumbblog/crumbblog/user/UserService.java
@@ -1,18 +1,22 @@
 package com.crumbblog.crumbblog.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     UserRepository userRepository;
+    BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         super();
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
