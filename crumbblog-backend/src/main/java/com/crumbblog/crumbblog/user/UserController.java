f7ffe082b734d4d3df1555f3a5d2c37ce49bc1e4
heidichiu <heidi@xqtem.com>
2021-03-12 13:29:40 +0800

    Add validation to user

--- crumbblog-backend/src/main/java/com/crumbblog/crumbblog/user/UserController.java
+++ crumbblog-backend/src/main/java/com/crumbblog/crumbblog/user/UserController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class UserController {
     @Autowired
     UserService userService;
 
     @PostMapping("/api/1.0/users")
-    GenericResponse createUser(@RequestBody User user) {
+    GenericResponse createUser(@Valid @RequestBody User user) {
         userService.save(user);
 
         return new GenericResponse("User saved");
